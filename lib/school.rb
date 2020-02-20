3833fa6c5834c87aa3fac61dbf54dacec71e06c0
norio c <norionorioc@gmail.com>
2020-02-20 00:52:29 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -25,17 +25,3 @@ class School
   end
 end 
 
-
-  def grade(student_grade)
-    roster[student_grade]
-  end
-
-  # this method should arrange the students in each grade by alphabetical order
-  def sort
-    sorted = {}
-    roster.each do |grade, students|
-      sorted[grade] = students.sort
-    end
-    sorted
-  end
-end
